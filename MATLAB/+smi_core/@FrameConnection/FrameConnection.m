32c303aa9fd92ef9d55e3867661df5f684330e45
dschodt <schodtd@unm.edu>
2020-09-03 15:41:44 -0600

    Add a basic skeleton for the class smi_core/FrameConnection

---
+++ MATLAB/+smi_core/@FrameConnection/FrameConnection.m
@@ -0,0 +1,48 @@
+classdef FrameConnection < handle
+    %FrameConnection contains methods for performing frame connection.
+    %
+    % REQUIRES:
+    %   c_FrameConnect.mex*
+    
+    % Created by: 
+    %   David J. Schodt (Lidke Lab 2020) 
+    %       based on a code written Hanieh Mazloom-Farsibaf
+    
+    
+    properties
+        LoS = 0.01; % level of significance
+        MaxSeparation = 1; % Maximum allowable separation between two 
+                           % localizations to still be considered 
+                           % candidates for connection.
+                           % (Pixels)(Default = 1 pixel)
+        MaxFrameGap = 5; % Maximum allowable frame gap between two 
+                         % localizations to still be considered 
+                         % candidates for connection.
+                         % (Pixels)(Default = 5 frames)
+        FitType = 'XYNB'; % Fit type used to fit localizations 
+                          % (see GaussMLE class for details)
+                          % (Character array)(Default = 'XYNB')
+        SMD; % Single Molecule Data structure (see 
+             % SingleMoleculeData class for details) set by the user.  This
+             % is the original, non frame-connected SMD structure.
+    end
+    
+    properties (SetAccess = 'protected')
+        SMDCombined; % Single Molecule Data structure (see 
+                     % SingleMoleculeData class for details) whose member
+                     % localizations have been frame-connected.
+                     % SMDCombined is the intended "output" to be prepared
+                     % by methods in this class.
+    end
+    
+    methods
+        function [obj] = FrameConnection(SMD, SMF)
+            %FrameConnection is a constructor for the FrameConnection class
+        end
+    end
+    
+    methods (Static)
+    end
+    
+    
+end
