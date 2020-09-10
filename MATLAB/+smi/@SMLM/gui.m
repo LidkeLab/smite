b8d27a8566375ab7b4a295ebe842cfd49a192714
Keith A. Lidke <kalidke@users.noreply.github.com>
2020-09-10 16:38:28 -0600

    SMLM Skeleton

---
+++ MATLAB/+smi/@SMLM/gui.m
@@ -0,0 +1,10 @@
+function gui(obj)
+% gui Interactive GUI for SMLM
+
+%file selection
+%button to open SMF editor
+%preset buttons? 
+%no need for 'show plots' since we save everything.  
+
+
+end
---
+++ MATLAB/+smi/@SMLM/testFit.m
@@ -0,0 +1,5 @@
+function testFit(obj,DataSetIndex)
+%testFit Detailed analysis and feedback of one dataset
+
+
+end
