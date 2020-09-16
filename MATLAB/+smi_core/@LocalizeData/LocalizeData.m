3b0559a057321edb06c54d0d5c1108b268a67422
dschodt <schodtd@unm.edu>
2020-09-16 16:39:41 -0600

    Add outputs to smi_core.LocalizeData.genLocalizations()+improve class constructor

--- MATLAB/+smi_core/@LocalizeData/LocalizeData.m
+++ MATLAB/+smi_core/@LocalizeData/LocalizeData.m
@@ -2,8 +2,12 @@ classdef LocalizeData < handle
     %LocalizeData contains methods used to find localizations in raw data.
     % This class contains method(s) to generate localizations from numeric
     % arrays of raw data in the form of images/stacks of images.
-    % 
     %
+    % EXAMPLE USAGE:
+    %   LD = smi_core.LocalizeData(RawData, SMF);
+    %   [SMD, ScaledData] = LD.genLocalizations();
+    %   OR
+    %   [~, SMD, ScaledData] = smi_core.LocalizeData(RawData, SMF, 1);
     %
     % REQUIRES:
     %   DipImage, to use joinchannels() in LocalizeData.genLocalizations().
@@ -26,14 +30,27 @@ classdef LocalizeData < handle
     end
     
     methods
-        function [obj] = LocalizeData(RawData, SMF)
+        function [obj, SMD, ScaledData] = LocalizeData(RawData, SMF, ...
+                AutoRun)
             %LocalizeData creates an instance of the LocalizeData class.
             % This method will prepare the LocalizeData class, setting
-            % inputs to class properties if provided.
+            % inputs to class properties if provided.  This constructor can
+            % also be used to run the main run method of the class, as long
+            % as RawData and SMF are provided.  To do so, set AutoRun = 1.
+            
+            
+            % Set a default for the AutoRun flag, which specifies whether
+            % or not we should run genLocalizations() in this constructor.
+            if (~exist('AutoRun', 'var') || isempty(AutoRun))
+                AutoRun = 0;
+            end
             
             % Set the RawData as a class property if provided.
             if (exist('RawData', 'var') && ~isempty(RawData))
                 obj.RawData = RawData;
+                AllFieldsSet = 1;
+            else
+                AllFieldsSet = 0;
             end
             
             % Set class properties based on the input SMF structure (if it
@@ -44,7 +61,7 @@ classdef LocalizeData < handle
                 SMF = smi_core.SingleMoleculeFitting.padSMF(SMF);
                 
                 % Set the desired SMF fields to class properties.
-               	obj.CameraType = SMF.Data.CameraType;
+                obj.CameraType = SMF.Data.CameraType;
                 obj.CameraGain = SMF.Data.CameraGain;
                 obj.CameraOffset = SMF.Data.CameraOffset;
                 obj.CameraReadNoise = SMF.Data.CameraReadNoise;
@@ -52,10 +69,25 @@ classdef LocalizeData < handle
                 obj.MinPhotons = SMF.BoxFinding.MinPhotons;
                 obj.PSFSigma = SMF.Fitting.PSFSigma;
                 obj.FitType = SMF.Fitting.FitType;
+            else
+                AllFieldsSet = 0;
+            end
+            
+            % Run genLocalizations() if desired.
+            if (AutoRun && AllFieldsSet)
+                [SMD, ScaledData] = obj.genLocalizations();
+            else
+                if (nargout() > 1)
+                    warning(['Constructor outputs SMD and ', ...
+                        'ScaledData were requested but ', ...
+                        'localizations were not generated.'])
+                end
+                SMD = [];
+                ScaledData = [];
             end
         end
         
-        genLocalizations(obj)
+        [SMD, ScaledData] = genLocalizations(obj)
         
     end
     
@@ -65,4 +97,3 @@ classdef LocalizeData < handle
     
     
 end
-
--- MATLAB/+smi_core/@LocalizeData/genLocalizations.m
+++ MATLAB/+smi_core/@LocalizeData/genLocalizations.m
@@ -1,4 +1,4 @@
-function genLocalizations(obj)
+function [SMD, ScaledData] = genLocalizations(obj)
 %genLocalizations generates localizations from raw data.
 % This method will generate localizations from an array of raw data.  This
 % is done by first finding candidate ROIs in the raw data that may contain
@@ -25,25 +25,25 @@ SMF.FindROI.PSFSigma = obj.PSFSigma;
 SMF.FindROI.MinPhotons = obj.MinPhotons;
 
 % Perform the gain and offset correction on the raw data.
-[obj.ScaledData] = smi_core.DataToPhotons.convertToPhotons(obj.RawData, ...
+[ScaledData] = smi_core.DataToPhotons.convertToPhotons(obj.RawData, ...
     obj.CameraGain, obj.CameraOffset, obj.CameraReadNoise);
                 
 % Generate candidate ROIs from the gain and offset corrected data.
-FindROI = smi_core.FindROI(SMF, obj.ScaledData);
-[ROIStack, obj.SMD] = FindROI.findROI();
+FindROI = smi_core.FindROI(SMF, ScaledData);
+[ROIStack, SMD] = FindROI.findROI();
 
 % Pass the candidate ROIs to the fitting algorithm.  The output SMD from
 % GaussMLE will contain localizations w.r.t. the ROIStack coordinates and
 % thus we need to convert back to the full field of view before proceeding.
 GaussMLE = smi_core.GaussMLE(SMF, ROIStack);
-[obj.SMD] = GaussMLE.gaussMLE(obj.SMD);
-obj.SMD.X = obj.SMD.X + obj.SMD.XBoxCorner;
-obj.SMD.Y = obj.SMD.Y + obj.SMD.YBoxCorner;
+[SMD] = GaussMLE.gaussMLE(SMD);
+SMD.X = SMD.X + SMD.XBoxCorner;
+SMD.Y = SMD.Y + SMD.YBoxCorner;
 
 % Set (but don't apply) the ThreshFlag in the SMD structure.
 MinMax.X_SE = [0, SMF.Thresholding.MaxXY_SE];
 MinMax.Y_SE = [0, SMF.Thresholding.MaxXY_SE];
-if ~isempty(obj.SMD.Z_SE)
+if ~isempty(SMD.Z_SE)
     MinMax.Z_SE = [0, SMF.Thresholding.MaxZ_SE];
 end
 MinMax.PValue = [SMF.Thresholding.MinPValue, 1];
@@ -52,7 +52,11 @@ MinMax.PSFSigma = [SMF.Thresholding.MinPSFSigma, ...
 MinMax.Photons = [SMF.Thresholding.MinPhotons, Inf];
 MinMax.Bg = [0, SMF.Thresholding.MaxBG];
 Threshold = smi_core.Threshold;
-[obj.SMD] = Threshold.setThreshFlag(obj.SMD, MinMax);
+[SMD] = Threshold.setThreshFlag(SMD, MinMax);
+
+% Store SMD and ScaledData as class properties.
+obj.SMD = SMD;
+obj.ScaledData = ScaledData;
 
 
 end
