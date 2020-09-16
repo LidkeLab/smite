52ee687d1e4821bed317e1920ea56a52f59e9c35
dschodt <schodtd@unm.edu>
2020-09-16 16:28:40 -0600

    Remove some print statements from smi_core.LocalizeData.genLocalizations()

--- MATLAB/+smi_core/@LocalizeData/genLocalizations.m
+++ MATLAB/+smi_core/@LocalizeData/genLocalizations.m
@@ -25,26 +25,22 @@ SMF.FindROI.PSFSigma = obj.PSFSigma;
 SMF.FindROI.MinPhotons = obj.MinPhotons;
 
 % Perform the gain and offset correction on the raw data.
-fprintf('LocalizeData.genLocalizations: gain/offset correcting data...\n')
 [obj.ScaledData] = smi_core.DataToPhotons.convertToPhotons(obj.RawData, ...
     obj.CameraGain, obj.CameraOffset, obj.CameraReadNoise);
                 
 % Generate candidate ROIs from the gain and offset corrected data.
-fprintf('LocalizeData.genLocalizations: finding candidate ROIs...\n')
 FindROI = smi_core.FindROI(SMF, obj.ScaledData);
 [ROIStack, obj.SMD] = FindROI.findROI();
 
 % Pass the candidate ROIs to the fitting algorithm.  The output SMD from
 % GaussMLE will contain localizations w.r.t. the ROIStack coordinates and
 % thus we need to convert back to the full field of view before proceeding.
-fprintf('LocalizeData.genLocalizations: fitting candidate ROIs...\n')
 GaussMLE = smi_core.GaussMLE(SMF, ROIStack);
 [obj.SMD] = GaussMLE.gaussMLE(obj.SMD);
 obj.SMD.X = obj.SMD.X + obj.SMD.XBoxCorner;
 obj.SMD.Y = obj.SMD.Y + obj.SMD.YBoxCorner;
 
 % Set (but don't apply) the ThreshFlag in the SMD structure.
-fprintf('LocalizeData.genLocalizations: generating threshold flags...\n')
 MinMax.X_SE = [0, SMF.Thresholding.MaxXY_SE];
 MinMax.Y_SE = [0, SMF.Thresholding.MaxXY_SE];
 if ~isempty(obj.SMD.Z_SE)
@@ -57,7 +53,6 @@ MinMax.Photons = [SMF.Thresholding.MinPhotons, Inf];
 MinMax.Bg = [0, SMF.Thresholding.MaxBG];
 Threshold = smi_core.Threshold;
 [obj.SMD] = Threshold.setThreshFlag(obj.SMD, MinMax);
-fprintf('LocalizeData.genLocalizations: done, localizations generated.\n')
 
 
 end
