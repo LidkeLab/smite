53042778306d9e3a724efee0f6c4fd69efdcffaa
MJWester <wester@salud.unm.edu>
2020-09-23 17:17:11 -0600

    Drift correction broken up into intra-/inter-dataset portions.

--- MATLAB/+smi_core/@DriftCorrection/driftCorrectKNNInter.m
+++ MATLAB/+smi_core/@DriftCorrection/driftCorrectKNNInter.m
@@ -1,9 +1,9 @@
 function [SMD, Statistics] = driftCorrectKNNInter(obj, SMD)
-%driftCorrectKNN calculates the drift directly from X,Y{,Z} coordinates
+%driftCorrectKNNInter calculates inter-dataset drift directly from X,Y{,Z} coordinates
 % by fitting a polynomial depending on time (i.e., frame number) to the frames
 % with each dataset (intra-dataset), and fitting constant shifts between
 % datasets (inter-dataset).  Fitting is done via performing fminsearch on the
-% (weighted) sums of nearest neighbor distances.
+% (weighted) sums of nearest neighbor distances.  Inter-dataset portion.
 %
 % INPUTS:
 %   SMD:         A structure with fields:
@@ -105,60 +105,24 @@ function [SMD, Statistics] = driftCorrectKNNInter(obj, SMD)
       DriftParams.NFrames     = obj.NFrames;
    end
 
-   % Initialize various parameters, either provided by the user or defaults.
-%  if exist('DriftParams', 'var')
-      % PixelSizeZUnit is needed for 3D to convert Z into the same units as X
-      % and Y.  PixelSizeZUnit is the X/Y pixel size in units of um.
-      if isfield(DriftParams, 'PixelSizeZUnit')
-         PixelSizeZUnit = DriftParams.PixelSizeZUnit;
-      else
-         PixelSizeZUnit = 0.1;   % default value in um
-      end
-      % L_intra is the intra-dataset threshold used in the cost function
-      % computation to limit spurious nearest neighbor distances.  L_intra is
-      % in units of pixels.
-      if isfield(DriftParams, 'L_intra')
-         L_intra = DriftParams.L_intra;
-      else
-         L_intra = 1;   % default value in pixels
-      end
-      % L_inter is the inter-dataset threshold used in the cost function
-      % computation to limit spurious nearest neighbor distances.  L_inter is
-      % in units of pixels.
-      if isfield(DriftParams, 'L_inter')
-         L_inter = DriftParams.L_inter;
-      else
-         L_inter = 2;   % default value in pixels
-      end
-      PDegree      = DriftParams.PDegree;
-      TolFun_intra = DriftParams.TolFun_intra;
-      TolX_intra   = DriftParams.TolX_intra;
-      TolFun_inter = DriftParams.TolFun_inter;
-      TolX_inter   = DriftParams.TolX_inter;
-      Init_inter   = DriftParams.Init_inter;
-
-      if any(isfield(DriftParams, {'NDatasets', 'NFrames'}))
-         SMD = ReorganizeDatasets(SMD, DriftParams);
-         % SMD.NFrames has changed, so if Init_inter was set to the old value,
-         % now reset it to the new value.
-         if Init_inter == NFrames_C
-            Init_inter = SMD.NFrames;
-         end
-      end
-%  else
-%     % Default values.
-%     L_intra       = 1;     % intra-dataset threshold
-%     L_inter       = 2;     % inter-dataset threshold
-%     PixelSizeZUnit = 0.1;  % pixel size in um
-%     PDegree       = 1;     % degree of the intra-dataset fitting polynomial
-%                            % for drift correction
-%     TolFun_intra = 1e-2;   % termination tolerance on the function value
-%     TolX_intra   = 1e-4;   % termination tolerance on the fitting polynomial
-%     TolFun_inter = 1e-2;   % termination tolerance on the function value
-%     TolX_inter   = 1e-4;   % termination tolerance on the fitting polynomial
-%     Init_inter   = SMD.NFrames; % initialization with respect to the previous
-%                            % dataset for inter-dataset drift correction
-%  end
+   % Initialize various parameters.
+   % PixelSizeZUnit is needed for 3D to convert Z into the same units as X
+   % and Y.  PixelSizeZUnit is the X/Y pixel size in units of um.
+   PixelSizeZUnit = DriftParams.PixelSizeZUnit;
+   % L_intra is the intra-dataset threshold used in the cost function
+   % computation to limit spurious nearest neighbor distances.  L_intra is
+   % in units of pixels.
+   L_intra = DriftParams.L_intra;
+   % L_inter is the inter-dataset threshold used in the cost function
+   % computation to limit spurious nearest neighbor distances.  L_inter is
+   % in units of pixels.
+   L_inter = DriftParams.L_inter;
+   PDegree      = DriftParams.PDegree;
+   TolFun_intra = DriftParams.TolFun_intra;
+   TolX_intra   = DriftParams.TolX_intra;
+   TolFun_inter = DriftParams.TolFun_inter;
+   TolX_inter   = DriftParams.TolX_inter;
+   Init_inter   = DriftParams.Init_inter;
 
    Statistics.Ndims          = Ndims;
    Statistics.PixelSizeZUnit = PixelSizeZUnit;
@@ -175,91 +139,12 @@ function [SMD, Statistics] = driftCorrectKNNInter(obj, SMD)
    Statistics.TolFun_inter   = TolFun_inter;
    Statistics.TolX_inter     = TolX_inter;
 
-   SMD.DriftX = zeros(SMD.NFrames, SMD.NDatasets, 'single');
-   SMD.DriftY = zeros(SMD.NFrames, SMD.NDatasets, 'single');
-   if Ndims == 3
-      SMD.DriftZ = zeros(SMD.NFrames, SMD.NDatasets, 'single');
-   end
-
    % ---------- Intra-Dataset drift correction --------------------------------
 
-   tic;
-   % These options will be used by fminsearch below.
-   options = optimset('TolFun', TolFun_intra, 'TolX', TolX_intra);
-
-   % Establish an indexing array to record rearrangements of points into
-   % datasets, so that this reordering can be later undone.
-   N = numel(SMD.X);
-   idx = zeros(N, 1);
-   base = 0;
-
-   SMRS = cell(1, SMD.NDatasets);
-   % Count the number of iterations and function calls.
-   it = 0;   fc = 0;
-   % Note that variables like X, Y, Z are vectors, while variables like XY are
-   % n x Ndims matrices.  The basic reason for combining vectors into matrices
-   % is to allow easier generalization to 3D.
-   for i = 1:SMD.NDatasets
-      mask = SMD.DatasetNum == i;
-      n = sum(mask);
-      XY = zeros(n, Ndims, 'single');
-      XY(:, 1) = SMD.X(mask);
-      XY(:, 2) = SMD.Y(mask);
-      if Ndims == 3
-         % Assume z values are given in um.  Convert these into the same units
-         % as the x and y values, which are assumed to be given in pixels.
-         % The z values are converted back to um on output.
-         XY(:, 3) = SMD.Z(mask) ./ PixelSizeZUnit;
-      end
-      FrameNum = single(SMD.FrameNum(mask));
-
-      idx(mask) = base + (1 : n);
-      base = base + n;
-
-      % Initialize polynomial fitting function, then optimize over the dataset.
-      % P0 and P are packed vectors containing coefficients for all Pdegree
-      % interpolations over all dimensions as required by fminsearch.  Note
-      % that the polynomial is of PDegree for the drift correction.
-      P0 = zeros(Ndims*PDegree, 1);
-      % Test for really sparse datasets.
-      if n <= 1
-         P = P0;
-         XYC = XY;
-      else
-         [P, ~, exitflag, output] = ...
-            fminsearch(@minD_intra, P0, options, XY, FrameNum, Ndims, L_intra);
-         if exitflag ~= 1
-            fprintf( ...
-               'driftCorrectKNN fminsearch on minD_intra exitflag = %d\n', ...
-                     exitflag);
-         end
-         it = it + output.iterations;
-         fc = fc + output.funcCount;
-
-         [~, XYC] = minD_intra(P, XY, FrameNum, Ndims, L_intra);
-      end
-
-      PX = P(1           : PDegree);
-      PY = P(PDegree + 1 : 2*PDegree);
-
-      % Values corrected for drift.
-      SMRS{i}.XY = XYC;
-      SMRS{i}.n  = n;
-
-      range = double(1:SMD.NFrames);
-      SMD.DriftX(:, i) = polyval([PX', 0], range);
-      SMD.DriftY(:, i) = polyval([PY', 0], range);
-
-      if Ndims == 3
-         PZ = P(2*PDegree + 1 : end);
-         SMD.DriftZ(:, i) = polyval([PZ', 0], range);
-      end
-   end
-   Statistics.Intra_iterations  = it;
-   Statistics.Intra_funcCount   = fc;
-   Statistics.Intra_elapsedTime = toc;
-
    % ---------- Inter-Dataset drift correction --------------------------------
+
+   idx  = obj.idx;
+   SMRS = obj.SMRS;
 
    tic;
    % These options will be used by fminsearch below.
@@ -384,50 +269,6 @@ end
 
 % =============================================================================
 
-function [sumNND, X] = minD_intra(Theta, X, T, Ndims, L_intra)
-% Sum of nearest neighbor distances for intra-dataset drift correction.
-
-   N = numel(Theta);
-
-   m = N/Ndims;
-   lo = 1;   hi = m;
-   for j = 1:Ndims
-      PX = Theta(lo : hi);
-      % Assume the constant term of the polynomial is zero for the frames
-      % within a dataset.  (This assumption considerably speeds up the
-      % intra-dataset minimization.)
-      X(:, j) = X(:, j) + polyval([PX', 0], T);
-
-      lo = lo + m;
-      hi = hi + m;
-   end
-
-   % D(:, 1) will be identically zero since here we are finding the nearest
-   % neighbors of X with respect to itself.
-   [~, D] = knnsearch(X, X, 'K', 2);
-   D2 = D(:, 2);
-
-   % Make distances > L_intra equal to L_intra in the cost function to
-   % de-emphasize widely separated nearest neighbors.  This can be be important
-   % in sparse datasets where long distances may separate localizations.  The
-   % nearest neighbor of a localization in one frame of a dataset may blink
-   % off, resulting in a totally different nearest neighbor for the same
-   % localization in the next frame.
-   sumNND = sum(min(D2, L_intra));
-
-   % In rare cases, D2 may contain NaNs, so here replace them with an average
-   % value (modeled on the code in minD_inter).
-   if isnan(sumNND)
-      nans = isnan(D2);
-      D2(nans) = [];
-      n = sum(nans);
-      sumNND = sum(min(D2, L_intra)) * (1 + n/numel(D2));
-   end
-
-end
-
-% -----------------------------------------------------------------------------
-
 function [sumNND, X2] = minD_inter(Theta, NS, X2, Ndims, L_inter)
 % Sum of nearest neighbor distances for inter-dataset drift correction.
 
@@ -454,61 +295,4 @@ function [sumNND, X2] = minD_inter(Theta, NS, X2, Ndims, L_inter)
       sumNND = sum(min(D, L_inter)) * (1 + n/numel(D));
    end
    
-end
-
-% =============================================================================
-
-function SMD = ReorganizeDatasets(SMD, DriftParams)
-% Reorganize the collected frames into user specified dataset divisions.
-
-   % NDatasets is the number of datasets.
-   % NFrames   is the number of frames per dataset.
-   % NFrames_total is the total number of frames over all datasets.
-   NFrames_total = SMD.NDatasets * SMD.NFrames;
-   if isfield(DriftParams, 'NDatasets') && ~isempty(DriftParams.NDatasets)
-      NDatasets = DriftParams.NDatasets;
-      NFrames   = NFrames_total / NDatasets;
-      if mod(NFrames, 1) ~= 0
-         error(['DriftParams.NDatasets (%d) does not divide evenly into\n', ...
-                'the total number of frames (%d)!'], NDatasets, NFrames_total);
-      end
-      if isfield(DriftParams, 'NFrames') && ~isempty(DriftParams.NFrames)
-         if DriftParams.NFrames ~= NFrames
-         error(['DriftParams.NDatasets * DriftParams.NFrames (%d * %d) !=\n',...
-                'the total number of frames (%d)!'], ...
-               DriftParams.NDatasets, DriftParams.NFrames, NFrames_total);
-         end
-      end
-   elseif isfield(DriftParams, 'NFrames') && ~isempty(DriftParams.NFrames)
-      NFrames   = DriftParams.NFrames;
-      NDatasets = NFrames_total / NFrames;
-      if mod(NDatasets, 1) ~= 0
-         error(['DriftParams.NFrames (%d) does not divide evenly into\n', ...
-                'the total number of frames (%d)!'], NFrames, NFrames_total);
-      end
-   end
-
-   % Compute absolute frame number as if there was only one dataset.
-   FrameNumAbs = ...
-      (SMD.DatasetNum - 1)*double(SMD.NFrames) + double(SMD.FrameNum);
-   % Reorganize the absolute frame numbers into new dataset divisions.
-   DatasetNum = ones(size(FrameNumAbs));
-   FrameNum   = zeros(size(FrameNumAbs));
-   for i = 1 : numel(DatasetNum)
-      DatasetNum(i) = ...
-         (FrameNumAbs(i) - 1 - mod(FrameNumAbs(i) - 1, NFrames)) / NFrames + 1;
-      FrameNum(i)   = mod(FrameNumAbs(i) - 1, NFrames) + 1;
-   end
-
-   % Save collected dataset/frame numbering.
-   SMD.Collected.NDatasets  = SMD.NDatasets;
-   SMD.Collected.NFrames    = SMD.NFrames;
-   SMD.Collected.DatasetNum = SMD.DatasetNum;
-   SMD.Collected.FrameNum   = SMD.FrameNum;
-   % Replace the old numbering scheme with the new one.
-   SMD.NDatasets  = NDatasets;
-   SMD.NFrames    = NFrames;
-   SMD.DatasetNum = DatasetNum;
-   SMD.FrameNum   = FrameNum;
-
 end
