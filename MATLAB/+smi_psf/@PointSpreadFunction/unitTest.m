30a7bdc9252df12db3f75aae260f6355adaf5f47
MJWester <wester@salud.unm.edu>
2020-09-29 10:40:58 -0600

    Fix PointSpreadFunction.unitTest calls.

--- MATLAB/+smi_psf/@PointSpreadFunction/unitTest.m
+++ MATLAB/+smi_psf/@PointSpreadFunction/unitTest.m
@@ -1,12 +1,12 @@
 function unitTest()
 %unitTest tests various functionality.
 
-   [CRLB]=crlbPSFPupil_unitTest();
-   [Report]=optimPSFZernike_unitTest();
-   [Report]=oversamplePSFPupil_unitTest();
-   [PSFStruct]=phaseRetrieval_unitTest();
-   [Report]=scalarPSFPrasadZone_unitTest();
-   psfROIStack_unitTest();
-   [Report]=zernikeImage_unitTest();
+   [CRLB]=smi_psf.PointSpreadFunction.crlbPSFPupil_unitTest();
+   [Report]=smi_psf.PointSpreadFunction.optimPSFZernike_unitTest();
+   [Report]=smi_psf.PointSpreadFunction.oversamplePSFPupil_unitTest();
+   [PSFStruct]=smi_psf.PointSpreadFunction.phaseRetrieval_unitTest();
+   [Report]=smi_psf.PointSpreadFunction.scalarPSFPrasadZone_unitTest();
+   smi_psf.PointSpreadFunction.psfROIStack_unitTest();
+   [Report]=smi_psf.PointSpreadFunction.zernikeImage_unitTest();
 
 end
