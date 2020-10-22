70515cd40a3b68bed0d2519fc573d37bccfb5876
MJWester <wester@salud.unm.edu>
2020-10-22 12:58:21 -0600

    Add in testFit and reorganize SMLM DatasetList.

--- MATLAB/+smi/@SMLM/SMLM.m
+++ MATLAB/+smi/@SMLM/SMLM.m
@@ -4,7 +4,7 @@ classdef SMLM < handle
     % This is a high-level class that provides complete analysis of SMLM data.
     %
     %
-    
+
     % =========================================================================
     properties
         SMF
@@ -16,11 +16,12 @@ classdef SMLM < handle
         ResultsDir  % (Default = 'DataDir/../Results/FileName/) same as Seq
     end
     % =========================================================================
-    
+
     % =========================================================================
     properties (Access=protected)
-        DC  % DriftCorrection class object used internally
-        SMD % SMD structure with final analysis results
+        DatasetList = [] % List of datasets to be analyzed
+        DC               % DriftCorrection class object used internally
+        SMD              % SMD structure with final analysis results
     end % properties (Access=protected)
     % =========================================================================
 
@@ -39,53 +40,70 @@ classdef SMLM < handle
                 obj.FileName = SMF.Data.FileName;
             end
         end
-        
+
+        % ---------------------------------------------------------------------
+
         function fullAnalysis(obj)
-            % fullAnalysis Analyze all data and save results
-            
+            % fullAnalysis analyzes all data and saves results.
+
             obj.analyzeAll();
             obj.saveResults();
-            
+
             %save
-            
+
+        end
+
+        % ---------------------------------------------------------------------
+
+        function testFit(obj, DatasetIndex)
+           %testFit performs detailed analysis and feedback of one dataset.
+
+           obj.DatasetList = DatasetIndex;
+           obj.analyzeAll();
+
         end
 
         % ---------------------------------------------------------------------
 
         function analyzeAll(obj)
             % analyzeAll loops over dataset and creates SMD.
-            
+
             % Define the list of datasets to be processed.
             obj.SMF = smi_core.LoadData.setSMFDatasetList(obj.SMF);
-            datasetList = obj.SMF.Data.DatasetList;
+            if isempty(obj.DatasetList)
+                obj.DatasetList = obj.SMF.Data.DatasetList;
+            else
+                % obj.DatasetList takes priority over what is in SMF.
+                obj.SMF.Data.DatasetList = obj.DatasetList;
+            end
 
-            % DriftCorrection class object is also used in analyzeDataset
+            % DriftCorrection class object is also used in analyzeDataset.
             obj.DC = smi_core.DriftCorrection(obj.SMF);
             obj.SMD=[];
-            for nn=1:numel(datasetList)
+            for nn=1:numel(obj.DatasetList)
                 SMDnn = obj.analyzeDataset(datasetList(nn), nn);
                 obj.SMD=smi_core.SingleMoleculeData.catSMD(obj.SMD,SMDnn);
             end
-            
+
             % Inter-dataset drift correction.
             if numel(datasetList) > 1
                fprintf('Drift correcting (inter-datastet) ...\n');
                obj.SMD = obj.DC.driftCorrectKNNInter(obj.SMD);
             end
         end
-        
+
         % ---------------------------------------------------------------------
-        
+
         function SMD=analyzeDataset(obj,DatasetIndex,DatasetCount)
-        % analyzeDataset Load and analyze one dataset    
+        % analyzeDataset Load and analyze one dataset
 
             if ~exist('DatasetCount', 'var')
                 DatasetCount = 1;
             end
-            
+
             fprintf('Loading dataset %d ...\n', DatasetIndex);
             [Dataset, obj.SMF]=obj.loadDataset(obj.SMF,DatasetIndex);
-            
+
             % Generate localizations from the current Dataset.
             LD = smi_core.LocalizeData(Dataset, obj.SMF);
             fprintf('Generating localizations ...\n');
@@ -94,15 +112,15 @@ classdef SMLM < handle
             % Define NDatasets, and DatasetNum from the dataset count.
             SMD.NDatasets  = 1;
             SMD.DatasetNum = DatasetCount * ones(size(SMD.FrameNum));
-            
+
             % Perform frame-connection on localizations in SMD.
-            if obj.SMF.FrameConnection.On 
+            if obj.SMF.FrameConnection.On
                 FC = smi_core.FrameConnection(SMD, obj.SMF);
                 [SMD] = FC.performFrameConnection();
             end
-            
+
             % Intra-dataset drift correction.
-            if obj.SMF.DriftCorrection.On 
+            if obj.SMF.DriftCorrection.On
                 fprintf('Drift correcting (intra-datastet) ...\n');
                 SMD = obj.DC.driftCorrectKNNIntra(SMD, DatasetIndex);
             end
@@ -112,7 +130,7 @@ classdef SMLM < handle
 
         function [Dataset, SMF]=loadDataset(obj,SMF,DatasetIndex)
         % loadDataset loads a dataset and converts to photons
-        % set obj.Data   
+        % set obj.Data
         LD = smi_core.LoadData;
         switch SMF.Data.FileType;
             case 'mat'
@@ -130,7 +148,7 @@ classdef SMLM < handle
         % saveResults saves all results and plots in subfolder.
         % gaussblobs, drift image, fits/frame, NumConnected hist,
         % Driftcorrection plots, precision hist, intensity hist,
-        % mat file with SMD and SMF files. 
+        % mat file with SMD and SMF files.
         if isempty(obj.SMD)
             error('No SMD results structure found to save!');
         end
@@ -157,5 +175,5 @@ classdef SMLM < handle
         Success = unitTest();
     end % methods(Static)
     % =========================================================================
-    
+
 end
--- MATLAB/+smi/@SMLM/testFit.m
+++
@@ -1,5 +0,0 @@
-function testFit(obj,DataSetIndex)
-%testFit Detailed analysis and feedback of one dataset
-
-
-end
