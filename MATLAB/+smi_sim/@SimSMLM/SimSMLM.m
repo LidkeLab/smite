c822f2ab0444649df80c60e8d43b0468b60e71da
MJWester <wester@salud.unm.edu>
2020-11-04 15:33:12 -0700

    Reorganization.

--- MATLAB/+smi_sim/@SimSMLM/SimSMLM.m
+++ MATLAB/+smi_sim/@SimSMLM/SimSMLM.m
@@ -24,95 +24,17 @@ classdef SimSMLM < handle
     
     methods 
         
-        function [SMD_True] = SimStar(obj,NWings)
-            
-            % This function simulates the Siemen star and returns the frames
-            % with particles distributed uniformaly on the wings of the star.
-            
-            % INPUTS:
-            
-            % NWings: The number of wings of the Siemen's star.
-            
-            % OUTPUTS:
-            
-            % [SMD]: This is a structure with the following fields:
-            
-            % SMD_True.X: The X-positions of particles generated randomly.
-            % (Number of the generated particles x 1)(Pixels)
-            
-            % SMD_True.Y: The Y-positions of particles generated randomly
-            % (Number of the generated particles x 1),(Pixels)
-            
-            % SMD_True.Z: The Z-positions of particles generated randomly
-            % (Number of the generated particles x 1), (um)
-            
-            % [Model]: The frames of the model (Row x Column x NFrames).
-            
-            % SMD_Model.X: The X-positions of the particles seen on the frames.
-            % (Number of the seen particles x 1),(Pixels)
-            
-            % SMD_Model.Y: The Y-positions of the particles seen on the frames.
-            % (Number of the seen particles x 1),(Pixels)
-            
-            % SMD_Model.Z: The Z-positions of particles (um)
-            
-            % SMD_Model.Photons: The intensity of the particles.
-            % (Number of the seen particles x 1),(Photon counts)
-            
-            % SMD_Model.FrameNum:The frames that the particles have been detected.
-            % (Number of the seen particles x 1)
-            
-            % SMD_Model.Bg: The background noise (1x1)(Photon counts)
-            
-            R = obj.SZ/3; %The length of the wing is a third of the size of the frame
-            
-            % To facilitate the calculations, we will work in both polar and
-            % Cartesian coordinates. The random particles will be generated
-            % throughout a cycle and then we remove those that are not
-            % inside any wing.
-            
-            Alpha = -pi:2*pi/NWings:pi; %The angle that each wing starts and ends.
-            Nn=poissrnd(floor(pi*R^2*obj.Rho)); % Nn are the number of emitters.
-            X = 2*(rand(Nn,1)-0.5);
-            Y = 2*(rand(Nn,1)-0.5);
-            IndZero=find(X.^2 + Y.^2>1);
-            X(IndZero)=0;
-            Y(IndZero)=0;
-            [Theta,~] = cart2pol(X,Y);
-            
-            for ij = 1:2:NWings
-                % i and j represent the ith particle in the jth frame.
-                BInd = find(Alpha(ij)<Theta & Theta<Alpha(ij+1));
-                X(BInd)=0;
-                Y(BInd)=0;
-            end
-            
-            % Delete those particles from the list that are not inside any wing.
-            DInd = find(X==0);
-            X(DInd)=[];
-            Y(DInd)=[];
-            LabelCoords(:,1)=X*R+obj.SZ/2;
-            LabelCoords(:,2)=Y*R+obj.SZ/2;
-            LabelCoords = LabelCoords*obj.ZoomFactor;
-            obj.SZ = obj.SZ*obj.ZoomFactor;
-            NLabels=length(LabelCoords); % Number of the generated particles.
-            IntArray=zeros(NLabels,obj.NFrames); % This is a 2D-array with
-            % the size of(Number of the particles)x(Number of the frames)
-            % to store the trace of the blinking events. The elements of
-            % this array can be either zero or one. One signifies an on event.
-            % Let say the element on the ith row and jth column is one. This
-            % signifies the ith particle is ON in the jth frame.
-            
-            %Saving the generated data in the structure SMD.
-            SMD_True.X = LabelCoords(:,1);
-            SMD_True.Y = LabelCoords(:,2);
-            if isscalar(obj.PSFSigma)
-                SMD_True.Z = [];
-            end
-        end
-        % Call the genBlinks () function to generate the model
+        [SMD_True] = simStar(obj,NWings)
+
+    end % methods
+
+    methods(Static)
+
+        % Call the genBlinks() function to generate the model
         [SMD_Model] = genBlinks(SMD_True,K_OnToOff,K_OffToOn,K_OnToBleach,NFrames,StartState)
-    end
+        unitTest()
+
+    end % methods(Static)
     
 end
 
