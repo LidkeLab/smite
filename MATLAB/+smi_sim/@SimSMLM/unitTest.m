b1871c86cd15dffe40deb3990102319449910744
MJWester <wester@salud.unm.edu>
2020-11-04 15:35:18 -0700

    Add files!

---
+++ MATLAB/+smi_sim/@SimSMLM/unitTest.m
@@ -0,0 +1,17 @@
+function unitTest()
+
+   obj=smi_sim.SimSMLM();
+   obj.SZ = 256;
+   obj.Rho=10;
+   obj.NFrames=10;
+   obj.ZoomFactor=1;
+   obj.K_OnToOff=1;
+   obj.K_OffToOn=0.005;
+   obj.K_OnToBleach=0.2;
+   obj.EmissionRate=1000;
+   obj.Bg=15;
+   obj.PSFSigma=1.3;
+   [SMD_Model] = obj.genBlinks(1,0.005,0.2,10,'Equib')
+   dipshow(SMD_Model)
+
+end
