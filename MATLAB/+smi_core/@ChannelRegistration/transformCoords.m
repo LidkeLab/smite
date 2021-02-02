dc80d4f6bfb0584a080d780c72638259cf86437e
dschodt <schodtd@unm.edu>
2021-02-02 09:51:12 -0700

    Revise usage of ChannelRegistration.transformCoords()

--- MATLAB/+smi_core/@ChannelRegistration/transformCoords.m
+++ MATLAB/+smi_core/@ChannelRegistration/transformCoords.m
@@ -1,30 +1,23 @@
-function [MovingCoordinates, FixedCoordinates] = transformCoords(...
-    RegistrationTransform, MovingCoordinates, FixedCoordinates)
+function [MovingCoordsTransformed] = ...
+    transformCoords(RegistrationTransform, MovingCoords)
 %transformCoords transforms a set of coordinates with the given transform.
 % This method transforms the input coordinates, with the set being
 % transformed depending on the type of transform.  The purpose of this
-% method is to eliminate the guesswork needed when applying the transform
-% (e.g., 'lwm' doesn't have a forward transform, so we have to do
-% "reverse" the effect of that transform). 
+% method is to overcome the annoyances/issues that may arise when applying 
+% the transform directly (e.g., 'lwm' doesn't have a forward transform, so
+% we have to do "reverse" the effect of that transform). 
 %
 % INPUTS:
 %   RegistrationTransform: RegistrationTransform is a MATLAB tform object
 %                          describing the transform to be visualized.
 %                          (tform object)
-%   MovingCoordinates: Coordinates of points in the second fiducial.
-%                      (Nx2 numeric array)
-%   FixedCoordinates: Coordinates of points in the first fiducial.
-%                     (Nx2 numeric array)
+%   MovingCoords: Coordinates of points in the "moving" fiducial.
+%                 (Nx2 numeric array)
 %
 % OUTPUTS:
-%   MovingCoordinates: Coordinates of points in the second fiducial.  These
-%                      coordinates may or may not be transformed, depending
-%                      on the type of transform used.
-%                      (Nx2 numeric array)
-%   FixedCoordinates: Coordinates of points in the first fiducial.  These
-%                     coordinates may or may not be transformed, depending
-%                     on the type of transform used.
-%                     (Nx2 numeric array)
+%   MovingCoordsTransformed: Input 'MovingCoords' transformed by 
+%                            'RegistrationTransform'. 
+%                            (Nx2 numeric array)
 
 % Created by:
 %   David J. Schodt (Lidke Lab, 2020)
@@ -39,14 +32,15 @@ if ismember(class(RegistrationTransform), ...
     % we'll need to "reverse" the transform at each point (i.e., use the
     % inverse transform, see how far points moved, and then move the
     % initial points the same amount in the opposite direction).
-    MovingCoordinatesTransformed = ...
+    MovingCoordsTransformed = ...
         smi_core.ChannelRegistration.transformCoordsDirect(...
-        RegistrationTransform, MovingCoordinates);
-    MovingCoordinates = 2*MovingCoordinates - MovingCoordinatesTransformed;
+        RegistrationTransform, MovingCoords);
+    MovingCoordsTransformed = 2*MovingCoords ...
+        - MovingCoordsTransformed;
 else
-    MovingCoordinates = ...
+    MovingCoordsTransformed = ...
         smi_core.ChannelRegistration.transformCoordsDirect(...
-        RegistrationTransform, MovingCoordinates);
+        RegistrationTransform, MovingCoords);
 end
 
 
