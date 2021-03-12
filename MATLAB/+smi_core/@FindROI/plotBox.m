247f5eec2a48a81290a5e89197974a720467db57
MJWester <wester@salud.unm.edu>
2021-03-12 10:16:02 -0700

    plotBox: red -> yellow so more obvious.

--- MATLAB/+smi_core/@FindROI/plotBox.m
+++ MATLAB/+smi_core/@FindROI/plotBox.m
@@ -19,10 +19,10 @@ YCorners = double(SMD.YBoxCorner(ParticleFrame));
 dipshow(Data(:,:,Frame))
 hold
 for nn = 1:length(ParticleFrame)
-   plot(XCorners(nn)*ones(1,BoxSize),YCorners(nn):YCorners(nn)+BoxSize-1,'r') 
-   plot((XCorners(nn)+BoxSize-1)*ones(1,BoxSize),YCorners(nn):YCorners(nn)+BoxSize-1,'r')
-   plot(XCorners(nn):XCorners(nn)+BoxSize-1,YCorners(nn)*ones(1,BoxSize),'r') 
-   plot(XCorners(nn):XCorners(nn)+BoxSize-1,(YCorners(nn)+BoxSize-1)*ones(1,BoxSize),'r')
+   plot(XCorners(nn)*ones(1,BoxSize),YCorners(nn):YCorners(nn)+BoxSize-1,'y') 
+   plot((XCorners(nn)+BoxSize-1)*ones(1,BoxSize),YCorners(nn):YCorners(nn)+BoxSize-1,'y')
+   plot(XCorners(nn):XCorners(nn)+BoxSize-1,YCorners(nn)*ones(1,BoxSize),'y') 
+   plot(XCorners(nn):XCorners(nn)+BoxSize-1,(YCorners(nn)+BoxSize-1)*ones(1,BoxSize),'y')
 end
 
 end
