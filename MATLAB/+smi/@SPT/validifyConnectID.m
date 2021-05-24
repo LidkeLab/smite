c1047c9655878d2f2a6dcca942511321d0bcd3ae
dschodt <schodtd@unm.edu>
2021-05-24 14:56:37 -0600

    Rewrite SPT.validifyConnectID() to improve speed

--- MATLAB/+smi/@SPT/validifyConnectID.m
+++ MATLAB/+smi/@SPT/validifyConnectID.m
@@ -2,8 +2,8 @@ function [ConnectID] = validifyConnectID(ConnectID)
 %validifyConnectID ensures ConnectID consists of the numbers 1:NConnectID
 % This method will convert an input ConnectID to the set of numbers
 % 1:numel(ConnectID).  For example, if 
-% ConnectID = [1, 2, 2, 4, 5, 5, 5, 7], the output will be 
-% [1, 2, 2, 3, 4, 4, 4, 5].
+% ConnectID = [1, 2, 2, 4, 5, 5, 5, 7].', the output will be 
+% [1, 2, 2, 3, 4, 4, 4, 5].'.
 %
 % INPUT:
 %   ConnectID: An array of integers.
@@ -19,12 +19,17 @@ function [ConnectID] = validifyConnectID(ConnectID)
 
 % Ensure ConnectID consists of the set of integers 1:numel(ConnectID)
 % without skipping any integers.
-ConnectIDList = unique(ConnectID);
+[ConnectIDSorted, SortIndices] = sort(ConnectID);
+ConnectIDList = unique(ConnectIDSorted);
+NLocPerID = groupcounts(ConnectIDSorted);
+NLocCumulative = [0; cumsum(NLocPerID)];
 NIDs = numel(ConnectIDList);
 for nn = 1:NIDs
     % Set all entries with ConnectID==ConnectIDList(nn) equal to nn.
-    ConnectID(ConnectID==ConnectIDList(nn)) = nn;
+    IndexArray = (1:NLocPerID(nn)) + NLocCumulative(nn);
+    ConnectIDSorted(IndexArray) = nn;
 end
+ConnectID(SortIndices) = ConnectIDSorted;
 
 
 end
