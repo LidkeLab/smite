03c1ef9da48cb545934a7d0f327f1604006b2669
dschodt <schodtd@unm.edu>
2021-06-29 19:51:55 -0600

    Revise comments in GenerateMovies.playMovie()

--- MATLAB/+smi_vis/@GenerateMovies/playMovie.m
+++ MATLAB/+smi_vis/@GenerateMovies/playMovie.m
@@ -12,65 +12,7 @@ function playMovie(PlotAxes, TR, RawData, Params, SMD, VideoObject)
 %            frame number.
 %            (Default = zeros(TR(1).YSize, TR(1).XSize, TR(1).NFrames))
 %   Params: Structure of display parameters that will be applied to
-%           the movie.
-%                  UnitFlag: Boolean flag to specify the units of the movie                            false for camera units (pixels, frames)
-%                            true for physical units (micrometers, seconds)
-%                            (boolean)(Default = false)
-%                  MaxTrajLength: Maximum number of points of a given
-%                                 trajectory that will be plotted.
-%                                 (frames)(Default = inf)
-%                  XPixels: Range of X values shown in movie.
-%                           ([MinPixel, MaxPixel])(pixels)
-%                           (Default = [1, XSize])
-%                  YPixels: Range of Y values shown in movie.
-%                           ([MinPixel, MaxPixel])(pixels)
-%                           (Default = [1, YSize])
-%                  ZFrames: Range of frames shown in movie.
-%                           ([MinFrame, MaxFrame])(frames)
-%                           (Default = [1, NFrames])
-%                  MinScaleIntensity: Minimum value that we will use to
-%                                     scale the raw data. If the raw data
-%                                     ranges from [0, MaxIntensity], we
-%                                     scale as RawData / MaxIntensity, but
-%                                     if raw data is too noisy this just
-%                                     brightens noise, so we set
-%                                     MaxIntensity = max(MaxIntensity,
-%                                     MinScaleIntensity).
-%                                     (numeric scalar)(Default = 1)
-%                  PercentileCeiling: Percentile ceiling of pixel values
-%                                     in the raw data above which values
-%                                     are clipped.
-%                                     (percentage)(Default = 100)
-%                  PlotMarker: Marker to be placed on top of each
-%                              localization. Options: MATLAB markers, e.g.,
-%                              'x' (see Line Properties in MATLAB
-%                              documentation) or 'none'.
-%                              (char. array)(Default = 'none')
-%                  SMDMarker: Marker to be placed on top of each
-%                             localization in SMD. The color is currently
-%                             hard coded to be red. (Default = '.')
-%                  LineOfSite: Line of site vector for view() (rotates
-%                              "camera" orientation for 3D movie.  When
-%                              Is2D = 0, LineOfSite can be given as an
-%                              NFramex2 array and the movie will rotate as
-%                              prescribed by these angles.
-%                              (1x2 array or NFramesx2 array)(degrees)
-%                              (Default = [0, 90])
-%                  Is2D: false if movie should be 3D (LineOfSite~=[0, 90])
-%                        true if movie should be 2D (might run faster for
-%                        long movies)(Default = true)
-%                  Resolution: Resolution of the movie that gets saved to
-%                              FilePath. This can only be used when
-%                              LiteMode = 1 (see usage of getframe() vs
-%                              print() in the actual code below).
-%                              (scalar)(dpi)(Default = 0, which sets to
-%                              screen resolution)
-%                  FrameRate: Approximate playback framerate used when the 
-%                             input 'VideoWriter' is not provided. 
-%                             (Default = 10 fps)
-%                  TrajColor: Color of each trajectory in TR.
-%                             (NTrajx3 numeric array)
-%                             (Default = lines(NTraj))
+%           the movie (see smi_vis.GenerateMovies.prepDefaults()).
 %   SMD: Single Molecule Data structure containing additional localizations
 %        that we want to show in the movie (e.g., this might be the results
 %        from a box finding algorithm, where the localizations aren't
