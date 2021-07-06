88936259c6e1b0cabef2f306094e98e537ffd7c7
dschodt <schodtd@unm.edu>
2021-07-06 17:26:36 -0600

    Revise documentation in smi_core.TrackingResults class

--- MATLAB/+smi_core/@TrackingResults/TrackingResults.m
+++ MATLAB/+smi_core/@TrackingResults/TrackingResults.m
@@ -11,41 +11,10 @@ classdef TrackingResults
     % structure (see smi_core.SingleMoleculeData) but organized in a more
     % user-friendly manner for SPT data.
     %
-    % The structure contains (at least) the following properties:
-    %   TrajectoryID: An integer identifier unique to each trajectory.
-    %   X: X position of the trajectory over time. (pixels)
-    %   Y: Y position of the trajectory over time. (pixels)
-    %   X_SE: The standard error of the X position estimate. (pixels)
-    %   Y_SE: The standard error of the Y position estimate. (pixels)
-    %   FrameNum: The frame in which localizations of the trajectory
-    %             appear. (frames)
-    %   Photons: The integrated photons found in each localization of
-    %            the trajectory. (photons)
-    %   Bg: The background found for each localization of the trajectory.
-    %       (photons)
-    %   PSFSigma: Estimated or fixed sigma of symmetric 2D Gaussian PSF
-    %             model (pixels)
-    %   PSFSigmaX: Estimated or fixed X sigma of symmetric 2D Gaussian PSF
-    %              model (pixels)
-    %   PSFSigmaY: Estimated or fixed Y sigma of symmetric 2D Gaussian PSF
-    %              model (pixels)
-    %   PSFSigma_SE: Standard error of PSFSigma (pixels)
-    %   PSFSigmaX_SE: Standard error of PSFSigmaX (pixels)
-    %   PSFSigmaY_SE: Standard error of PSFSigmaY (pixels)
-    %   IndTD: An index back into the associated TD structure, i.e.,
-    %          TR(nn).X(ii) can be found in TD.X(TR(nn).IndTD(ii)).
-    %   FrameRate: The frame rate used to acquire this data.
-    %              (frames / second)
-    %   PixelSize: The pixel size of the sensor used to acquire this data
-    %              BACK PROJECTED to the sample plane. (micrometers)
-    %   DataROI: ROI of original raw data in which the trajectories were
-    %            found, formatted as [YStart, XStart, YEnd, XEnd]. (pixels)
-    %   XSize: The number of pixels along the X dimension of the ROI.
-    %          (pixels)
-    %   YSize: The number of pixels along the Y dimensino of the ROI.
-    %          (pixels)
-    %   FileDir: Directory containing the raw data corresponding to this TR
-    %   FileName: Name of the file corresponding to this TR.
+    % The TR structure is just an array of SMD structures, with each array
+    % element being an SMD structure corresponding to a single trajectory.
+    % Some additional fields, such as 'FileDir' and 'FileName', are also
+    % present in TR structures.
     %
     % SEE ALSO:
     %   smi_core.SingleMoleculeData, smi_core.SingleMoleculeFitting
@@ -57,26 +26,7 @@ classdef TrackingResults
     methods (Static)
         function [TR] = createTR()
             %createTR creates an empty Tracking Results (TR) structure.
-            TR.TrajectoryID = [];
-            TR.X = [];
-            TR.Y = [];
-            TR.X_SE = [];
-            TR.Y_SE = [];
-            TR.FrameNum = [];
-            TR.Photons = [];
-            TR.Bg = [];
-            TR.PSFSigma = [];
-            TR.PSFSigmaX = [];
-            TR.PSFSigmaY = [];
-            TR.PSFSigma_SE = [];
-            TR.PSFSigmaX_SE = [];
-            TR.PSFSigmaY_SE = [];
-            TR.IndTD = [];
-            TR.FrameRate = [];
-            TR.PixelSize = [];
-            TR.DataROI = [];
-            TR.XSize = [];
-            TR.YSize = [];
+            TR = smi_core.SingleMoleculeData.createSMD();
             TR.FileDir = '';
             TR.FileName = '';
         end
