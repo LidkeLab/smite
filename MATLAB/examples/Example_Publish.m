4aedb38663fc63c1f5070a17070626f74cf98d34
MJWester <wester@salud.unm.edu>
2021-07-13 10:21:08 -0600

    Updated comments in Example_Publish.

--- MATLAB/examples/Example_Publish.m
+++ MATLAB/examples/Example_Publish.m
@@ -2,16 +2,17 @@
 % experiment on the sequential microscope.  
 
 % Define the analysis parameters.
+% Directory containing the Cell*\Label*\Data*.h5 sub-directories:
 CoverslipDir = 'Y:\dschodt\analysis_test_directory\SMA_Publish_testing\21_5_4_HeLa_alpha_beta_tubulin';
 SMF = smi_core.SingleMoleculeFitting;
 SMF.Data.CameraType = 'SCMOS';
 SMF.Data.CalibrationFilePath = ['Y:\sCMOS Calibrations\Sequential SR\', ...
     'GainCalibration-2015-12-10-17-19-23.mat'];
 SMF.Data.PixelSize = 0.0954; % microns
-SMF.BoxFinding.BoxSize = 8;
+SMF.BoxFinding.BoxSize = 8; % pixels
 SMF.Fitting.FitType = 'XYNB';
-SMF.Fitting.PSFSigma = 1.3;
-SMF.Thresholding.MaxXY_SE = 0.15;
+SMF.Fitting.PSFSigma = 1.3; % pixels
+SMF.Thresholding.MaxXY_SE = 0.15; % pixels
 SMF.Thresholding.MinPhotons = 200;
 SMF.FrameConnection.Method = 'LAP-FC';
 
