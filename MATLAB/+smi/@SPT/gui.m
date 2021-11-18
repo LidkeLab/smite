917cf09674af94babba0643ad479842ea9fe9981
dschodt <35635770+dschodt@users.noreply.github.com>
2021-11-17 20:04:13 -0700

    Add changes omitted from last commit

--- MATLAB/+smi/@SPT/gui.m
+++ MATLAB/+smi/@SPT/gui.m
@@ -29,6 +29,62 @@ obj.SMF.gui(SMFPanel);
 TextSize = [0, 0, 0.2, 0.2];
 EditSize = [0, 0, 0.1, 0.2];
 ButtonSize = [0, 0, 0.2, 0.2];
+ControlHandles.Track = uicontrol(ControlPanel, ...
+    'Style', 'pushbutton', 'String', 'Track', ...
+    'FontUnits', 'normalized', 'FontSize', 0.4, ...
+    'Units', 'normalized', ...
+    'Position', ButtonSize, ...
+    'callback', @track);
+ControlHandles.TestFitButton = uicontrol(ControlPanel, ...
+    'Style', 'pushbutton', 'String', 'Test Track', ...
+    'FontUnits', 'normalized', 'FontSize', 0.4, ...
+    'Units', 'normalized', ...
+    'Position', ButtonSize + [0, ButtonSize(4), 0, 0], ...
+    'callback', @testTrack);
+ControlHandles.MakeMovie = uicontrol(ControlPanel, ...
+    'Style', 'pushbutton', 'String', 'Movie GUI', ...
+    'FontUnits', 'normalized', 'FontSize', 0.4, ...
+    'Units', 'normalized', ...
+    'Position', ButtonSize + [ControlPanel.Position(3)-ButtonSize(3), 0, 0, 0], ...
+    'callback', @makeMovieGUI);
+
+    function track(~, ~)
+        % Track the data based on the SMF GUI parameters.
+        if isfile(fullfile(obj.SMF.Data.FileDir, obj.SMF.Data.FileName{1}))
+            obj.performFullAnalysis()
+        else
+            error('%s cannot be found', ...
+                fullfile(obj.SMF.Data.FileDir, obj.SMF.Data.FileName{1}))
+        end
+    end
+
+    function testTrack(~, ~)
+        % Track the data based on the SMF GUI parameters.
+        if isfile(fullfile(obj.SMF.Data.FileDir, obj.SMF.Data.FileName{1}))
+            TestFlagInit = obj.IsTestRun;
+            obj.IsTestRun = true;
+            try
+                obj.performFullAnalysis()
+            catch MException
+                obj.IsTestRun = TestFlagInit;
+                rethrow(MException)
+            end
+            obj.IsTestRun = TestFlagInit;
+        else
+            error('%s cannot be found', ...
+                fullfile(obj.SMF.Data.FileDir, obj.SMF.Data.FileName{1}))
+        end
+    end
+
+    function makeMovieGUI(~, ~)
+        % Prepare the movie maker.
+        MovieMaker = smi_vis.GenerateMovies;
+        MovieMaker.TR = obj.TR;
+        MovieMaker.SMD = obj.SMD;
+        MovieMaker.RawData = obj.ScaledData;
+        MovieMaker.SMF = copy(obj.SMF);
+        MovieMaker.gui()
+    end
 
 
 end
