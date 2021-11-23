e1a846c849777394d84a9d4848ee976af4905be6
dschodt <35635770+dschodt@users.noreply.github.com>
2021-11-22 17:41:08 -0700

    Update Example_DiffusionEstimator.m

--- MATLAB/examples/Example_DiffusionEstimator.m
+++ MATLAB/examples/Example_DiffusionEstimator.m
@@ -2,31 +2,23 @@
 
 %% Single diffusing population, estimating D as the slope of the MSD.
 % Simulate some trajectories.
-% NOTE: For now, this requires sma-core-alpha to use
-%       SMA_Sim.simulateTrajectories(), which hasn't been ported to smite.
-SimParams.AllowOligomers = 0; % allows higher order oligomers
-SimParams.ParticleDensity = 0.005; % particles / px^2
-SimParams.NSubFrames = 1000;
+SimParams = smi_sim.SimSPT.defineDefaultParams();
+SimParams.ParticleDensity = 0.01; % particles / px^2
 SimParams.NFrames = 1000;
-SimParams.FrameSize = 128; % pixels (square only)
-SimParams.PixelSize = 0.1;
-SimParams.FrameRate = 20;
+SimParams.SubframeDensity = 1;
+SimParams.FrameSize = [128, 128];
 SimParams.BoundaryCondition = 'Periodic';
-SimParams.KBlinkOn = 1;
-SimParams.KBlinkOff = 0;
-SimParams.D = 0.0123 ...
-    / (SimParams.FrameRate * SimParams.PixelSize^2); % px^2 / subframe
-SimParams.Intensity = 300;
-[SMD, ~, ~, SimParams] = SMA_Sim.simulateTrajectories(SimParams);
-SMD.ConnectID = SMD.TrajectoryID; % fix for SMA_Sim.simulateTrajectories()
-TR = smi_core.TrackingResults.convertSMDToTR(SMD);
+SimParams.KOffToOn = 1;
+SimParams.KOnToOff = 0;
+SimParams.D = 0.0123; % px^2 / frame
+SPTSim = smi_sim.SimSPT(SimParams);
+SPTSim.createSimulation();
+TR = SPTSim.TR;
 
 % Prepare the DiffusionEstimator class and estimate D.
 % NOTE: For MSD fitting, the important parameters are 'DiffusionModel', 
 %       'FitMethod', 'FrameLagRange', and 'NFitPoints'.  
 SMF = smi_core.SingleMoleculeFitting; % used for PixelSize and FrameRate
-SMF.Data.PixelSize = SimParams.PixelSize;
-SMF.Data.FrameRate = SimParams.FrameRate;
 DE = smi_stat.DiffusionEstimator(TR, SMF);
 DE.FitTarget = 'MSD';
 DE.DiffusionModel = 'Brownian1C'; % must be 'Brownian1C' for MSD fitting
@@ -35,7 +27,7 @@ DE.FrameLagRange = [1, 50]; % range of frame lags computed in MSD
 DE.NFitPoints = 5; % # of points in MSD used for fit.
 DE.EstimateSEs = true; % estimate standard errors of fit D values
 DE.FitIndividualTrajectories = true; % fit each trajectory MSD
-DE.UnitFlag = true;
+DE.UnitFlag = false;
 DE.Verbose = 1;
 DE.estimateDiffusionConstant();
 
@@ -45,38 +37,30 @@ DE.plotEnsembleMSD(axes(figure()), DE.MSDEnsemble, DE.DiffusionStruct, ...
 
 %% Two diffusing populations, estimating D by fitting the CDF of jumps.
 % Simulate some trajectories.
-% NOTE: For now, this requires sma-core-alpha to use
-%       SMA_Sim.simulateTrajectories(), which hasn't been ported to smite.
-SimParams.AllowOligomers = 0; % allows higher order oligomers
-SimParams.ParticleDensity = 0.005; % particles / px^2
-SimParams.NSubFrames = 1000;
+SimParams = smi_sim.SimSPT.defineDefaultParams();
+SimParams.ParticleDensity = 0.01; % particles / px^2
 SimParams.NFrames = 1000;
-SimParams.FrameSize = 128; % pixels (square only)
-SimParams.PixelSize = 0.1;
-SimParams.FrameRate = 20;
+SimParams.SubframeDensity = 1;
+SimParams.FrameSize = [128, 128];
 SimParams.BoundaryCondition = 'Periodic';
-SimParams.KBlinkOn = 1;
-SimParams.KBlinkOff = 0;
-SimParams.D = [0.0123, 0.00321, 0.00321] ...
-    / (SimParams.FrameRate * SimParams.PixelSize^2); % px^2 / subframe
-SimParams.Intensity = 300;
-[SMD, ~, ~, SimParams] = SMA_Sim.simulateTrajectories(SimParams);
-SMD.ConnectID = SMD.TrajectoryID; % fix for SMA_Sim.simulateTrajectories()
-TR = smi_core.TrackingResults.convertSMDToTR(SMD);
+SimParams.KOffToOn = 1;
+SimParams.KOnToOff = 0;
+SimParams.D = [0.0123, 0.00321, 0.00321]; % px^2 / frame
+SPTSim = smi_sim.SimSPT(SimParams);
+SPTSim.createSimulation();
+TR = SPTSim.TR;
 
 % Prepare the DiffusionEstimator class and estimate D.
 % NOTE: For CDF fitting, the important parameters are 'DiffusionModel' and
 %       'FrameLagRange'.
 SMF = smi_core.SingleMoleculeFitting; % used for PixelSize and FrameRate
-SMF.Data.PixelSize = SimParams.PixelSize;
-SMF.Data.FrameRate = SimParams.FrameRate;
 DE = smi_stat.DiffusionEstimator(TR, SMF);
 DE.FitTarget = 'CDFOfJumps';
 DE.DiffusionModel = 'Brownian2C';
-DE.FrameLagRange = [2, 2]; % range of frame lags computed in MSD
+DE.FrameLagRange = [1, 5]; % range of frame lags computed in MSD
 DE.EstimateSEs = false; % estimate standard errors of fit D values
 DE.FitIndividualTrajectories = false; % fit each trajectory MSD
-DE.UnitFlag = true;
+DE.UnitFlag = false;
 DE.Verbose = 1;
 DE.estimateDiffusionConstant();
 
@@ -92,14 +76,12 @@ DE.plotEnsembleCDFOfJumps(axes(figure()), ...
 % NOTE: For LikelihoodOfJumps maximization, the important parameters are
 %       'DiffusionModel' and 'FrameLagRange'.
 SMF = smi_core.SingleMoleculeFitting; % used for PixelSize and FrameRate
-SMF.Data.PixelSize = SimParams.PixelSize;
-SMF.Data.FrameRate = SimParams.FrameRate;
 DE = smi_stat.DiffusionEstimator(TR, SMF);
 DE.FitTarget = 'LikelihoodOfJumps';
 DE.DiffusionModel = 'Brownian2C';
-DE.FrameLagRange = [2, 2]; % range of frame lags computed in MSD
+DE.FrameLagRange = [1, inf]; % range of frame lags computed in MSD
 DE.EstimateSEs = true; % estimate standard errors of fit D values
 DE.FitIndividualTrajectories = false; % fit each trajectory MSD
-DE.UnitFlag = true;
+DE.UnitFlag = false;
 DE.Verbose = 1;
 DE.estimateDiffusionConstant();
