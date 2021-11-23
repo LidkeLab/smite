ca03460e06ff6cfc752028c3a48848dda198f2fc
MJWester <wester@salud.unm.edu>
2021-11-23 15:25:08 -0700

    More dipshow -> sliceViewer

--- MATLAB/+smi_psf/@PointSpreadFunction/oversamplePSFPupil_unitTest.m
+++ MATLAB/+smi_psf/@PointSpreadFunction/oversamplePSFPupil_unitTest.m
@@ -11,12 +11,12 @@ P.Z=(-2:.02:2)
 P.OTFSigma=[.1,.1];
 %Show unsampled PSF
 [PSF]=smi_psf.PointSpreadFunction.scalarPSFPupil(P);
-dipshow(gather(PSF))
+sliceViewer(gather(PSF))
 colormap('hot')
 
 [PFS_OS,P_OS]=smi_psf.PointSpreadFunction.oversamplePSFPupil(P,4);
 %Show upsampled PSF
-dipshow(gather(PFS_OS))
+sliceViewer(gather(PFS_OS))
 colormap('hot')
 
 Report = 1;
