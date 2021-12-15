85a0f6c4b1eaa5ef33a573ca2cc62f30b4589ad0
MJWester <wester@salud.unm.edu>
2021-12-14 18:32:44 -0700

    Use new function fitsPerFrame in SMLM.

--- MATLAB/+smi/@SMLM/generatePlots.m
+++ MATLAB/+smi/@SMLM/generatePlots.m
@@ -156,18 +156,6 @@ if isfield(SMD,'DriftX') && ~isempty(SMD.DriftX) && ...
 end
 
 if ismember("FitFrame", PlotDo)
-   Nloc_frame = [];
-   % Number of localizations per frame
-   for jj=1:max(SMD.DatasetNum)
-       for ii=1:max(SMD.FrameNum)
-           idx=find(SMD.FrameNum==ii & SMD.DatasetNum==jj);
-           Nloc_frame{jj}(ii)=length(idx);
-       end
-   end
-   FitFrame = Nloc_frame{1};
-   for ii=1:length(Nloc_frame)-1
-       FitFrame=cat(2, FitFrame, Nloc_frame{ii+1});
-   end
    %FitFrame = smi.SMLM.fitsPerFrame(SMD);
 
    % plot fits per frame
