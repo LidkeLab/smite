16088b8baf2bc65524c2356ae4ad32ed76303aa7
dschodt <35635770+dschodt@users.noreply.github.com>
2022-03-02 11:25:23 -0700

    Fix a broken unit test in smi.SPT

--- MATLAB/+smi/@SPT/unitTestFFGC.m
+++ MATLAB/+smi/@SPT/unitTestFFGC.m
@@ -9,16 +9,18 @@ function [Success] = unitTestFFGC()
 
 % Simulate Tracking Data (create a simulated SMD structure).
 SPTSim = smi_sim.SimSPT;
+SPTSim.SimParams.ParticleDensity = 0.01;
+SPTSim.SimParams.Intensity = 500;
 SPTSim.createSimulation()
 SMD = SPTSim.SMD;
 SMF = smi_core.SingleMoleculeFitting;
 SMF.Data.DataROI = [1, 1, SMD.YSize, SMD.XSize];
 SMF.Fitting.PSFSigma = 1.3;
-[~, RawData] = smi_sim.GaussBlobs.gaussBlobImage(SMD, SMF);
+[~, RawData] = smi_sim.GaussBlobs.gaussBlobImage(SMD, SMF, 5);
 
 % Perform the frame-to-frame connection process.
 SMF = smi_core.SingleMoleculeFitting();
-SMF.Tracking.D = SPTSim.SimParams.D;
+SMF.Tracking.D = SPTSim.SimParams.D * ones(size(SMD.FrameNum));
 SMF.Tracking.K_off = SPTSim.SimParams.KOnToOff;
 SMF.Tracking.K_on = SPTSim.SimParams.KOffToOn;
 SMF.Tracking.MaxFrameGap = 20;
@@ -30,13 +32,13 @@ RhoOnMean = mean(smi_core.SingleMoleculeData.computeDensity(SMD));
 RhoOffMean = (SPTSim.SimParams.KOnToOff/SPTSim.SimParams.KOffToOn) * RhoOnMean;
 SMF.Tracking.Rho_off = RhoOffMean;
 for ff = min(SMD.FrameNum):(max(SMD.FrameNum)-1)
-    [CM] = smi.SPT.createCostMatrixFF(SMD, SMF, [], ff, -1);
+    [CM] = smi.SPT.createCostMatrixFF(SMD, SMF, RhoOffMean, ff, -1);
     [Link12] = smi.SPT.solveLAP(CM);
     [SMD] = smi.SPT.connectTrajFF(SMD, Link12, ff);
 end
 
 % Perform the gap-closing process.
-[CM] =smi.SPT.createCostMatrixGC(SMD, SMF, [], -1, 1); 
+[CM] =smi.SPT.createCostMatrixGC(SMD, SMF, RhoOffMean, -1, 1); 
 [Link12] = smi.SPT.solveLAP(CM);
 [SMD] = smi.SPT.connectTrajGC(SMD, Link12);
 TR = smi_core.TrackingResults.convertSMDToTR(SMD);
