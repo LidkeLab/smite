177d4b28863203c696b26b1a73dff3ed66bb2e06
MJWester <wester@salud.unm.edu>
2022-03-30 12:46:30 -0600

    New call to writeMPEG4.

--- MATLAB/+smi_core/@LocalizeData/genLocalizations.m
+++ MATLAB/+smi_core/@LocalizeData/genLocalizations.m
@@ -73,29 +73,34 @@ if obj.Verbose >= 3
    if isempty(obj.ResultsDir)
       obj.colorOverlay();
    else
-      islinux = isunix && ~ismac;
-      RGBout = fullfile(obj.ResultsDir, 'LocalizeDataRGBImage');
+      ResultsDir = obj.ResultsDir;
+      FileBaseName = 'LocalizeDataRGBImage';
       RGBImage = obj.colorOverlay();
-      RGBImageReordered = permute(RGBImage, [1, 2, 4, 3]);
-      % VideoWriter in Linux cannot generate .mp4 files, so it necessary to
-      % save in a different format and convert to .mp4 via external software
-      % (ffmpeg), which, of course, must be installed.  ffmpeg can convert
-      % between a variety of video formats.
-      if ~islinux
-         v = VideoWriter(RGBout, 'MPEG-4');
-      else
-         v = VideoWriter(RGBout, 'Motion JPEG AVI');
-      end
-      open(v);
-      writeVideo(v, RGBImageReordered);
-      close(v);
-      if islinux
-         cmd = sprintf('ffmpeg -y -i %s.avi %s.mp4', RGBout, RGBout);
-         [status, result] = system(cmd);
-         if status ~= 0
-            result
-         end
-      end
+      smi_helpers.writeMPEG4(ResultsDir, FileBaseName, RGBImage);
+
+%     islinux = isunix && ~ismac;
+%     RGBout = fullfile(obj.ResultsDir, 'LocalizeDataRGBImage');
+%     RGBImage = obj.colorOverlay();
+%     RGBImageReordered = permute(RGBImage, [1, 2, 4, 3]);
+%     % VideoWriter in Linux cannot generate .mp4 files, so it necessary to
+%     % save in a different format and convert to .mp4 via external software
+%     % (ffmpeg), which, of course, must be installed.  ffmpeg can convert
+%     % between a variety of video formats.
+%     if ~islinux
+%        v = VideoWriter(RGBout, 'MPEG-4');
+%     else
+%        v = VideoWriter(RGBout, 'Motion JPEG AVI');
+%     end
+%     open(v);
+%     writeVideo(v, RGBImageReordered);
+%     close(v);
+%     if islinux
+%        cmd = sprintf('ffmpeg -y -i %s.avi %s.mp4', RGBout, RGBout);
+%        [status, result] = system(cmd);
+%        if status ~= 0
+%           result
+%        end
+%     end
    end
 end
 
