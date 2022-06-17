552f29181958ec609b9a7da258ea62b910430e46
dschodt <35635770+dschodt@users.noreply.github.com>
2022-06-17 13:54:48 -0600

    Fix an array math bug in smi_stat.HMM

--- MATLAB/+smi_stat/@HMM/generateEmissionPDFs.m
+++ MATLAB/+smi_stat/@HMM/generateEmissionPDFs.m
@@ -84,7 +84,7 @@ end
         SeparationDimer = X{5};
         
         % Define the dimer state pdf.
-        VarianceDimer = sum(PositionSE.^2, 2) + SigmaOverlay^2;
+        VarianceDimer = sum(PositionSE.^2, 2) + SigmaOverlay.^2;
         DimerPDF = (Separation./VarianceDimer) ...
             .* exp(-0.5*(Separation.^2+SeparationDimer^2)./VarianceDimer) ...
             .* besseli(0, Separation*SeparationDimer./VarianceDimer);
@@ -120,7 +120,7 @@ end
         SeparationDomain = X{8};
         
         % Define the domain state pdf.
-        VarianceDomain = sum(PositionSE.^2, 2) + SigmaOverlay^2;
+        VarianceDomain = sum(PositionSE.^2, 2) + SigmaOverlay.^2;
         DomainPDF = (Separation./VarianceDomain) ...
             .* exp(-0.5*(Separation.^2+SeparationDomain^2)./VarianceDomain) ...
             .* besseli(0, Separation*SeparationDomain./VarianceDomain);
