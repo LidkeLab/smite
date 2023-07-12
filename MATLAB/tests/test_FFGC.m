3f2c2b8f79fcfa0b02cd67ac8d18c781d3b47dfd
MJWester <wester@salud.unm.edu>
2023-07-12 15:33:36 -0600

    Remove the tests directory in preparation to replace wth test_core.m

--- MATLAB/tests/test_FFGC.m
+++
@@ -1,9 +0,0 @@
-% Run various tests on the core functionality of SMITE.  Much output will be
-% saved in tempdir/smite/unitTest/name_of_test.  ExpectedResults are provided
-% in the directory in which run_tests.m resides where very large files have
-% been deleted so as to not bloat up the the SMITE distribution.
-
-% +smi
-
-fprintf('smi.SPT.unitTestFFGC (frame-to-frame and gap closing processes)\n');
-smi.SPT.unitTestFFGC()
