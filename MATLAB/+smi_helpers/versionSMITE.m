5c0157607c2de4ce24c7dc8d58c47315440700e6
MJWester <wester@salud.unm.edu>
2023-09-07 13:36:19 -0600

    JOSS paper Version 1.0.0!

--- MATLAB/+smi_helpers/versionSMITE.m
+++ MATLAB/+smi_helpers/versionSMITE.m
@@ -1,7 +1,7 @@
 function version = versionSMITE()
 % The version (or release) of SMITE currenly being used.
 
-   version = 'development';
-   %version = '0.3.0';
+   %version = 'development';
+   version = '1.0.0';
 
 end
