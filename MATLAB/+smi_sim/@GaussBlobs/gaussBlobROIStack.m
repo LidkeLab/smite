051d6426baf46548ae24086d0bb5391afd5f5ed8
Michael Wester <wester@math.unm.edu>
2023-09-21 16:55:53 -0600

    Missing SMD.

--- MATLAB/+smi_sim/@GaussBlobs/gaussBlobROIStack.m
+++ MATLAB/+smi_sim/@GaussBlobs/gaussBlobROIStack.m
@@ -118,7 +118,7 @@ for ii = 1:Nloops
     if ~isempty(SMD.PSFSigma)
         xSigma = single(SMD.PSFSigma(startFr:endFr,2));
         ySigma = single(SMD.PSFSigma(startFr:endFr,1));
-    elseif ~isempty(SMD.PSFSigmaX) && ~isempty(PSFSigmaY)
+    elseif ~isempty(SMD.PSFSigmaX) && ~isempty(SMD.PSFSigmaY)
         xSigma = single(SMD.PSFSigmaX(startFr:endFr,2));
         ySigma = single(SMD.PSFSigmaY(startFr:endFr,1));
     else
