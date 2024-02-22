85f2162918e4e866f4356f41dee199a9d66778bb
Michael Wester <wester@math.unm.edu>
2024-02-21 17:06:28 -0700

    Replace StatisticsClustering unitTest results by good plots!

--- MATLAB/+smi_helpers/versionSMITE.m
+++ MATLAB/+smi_helpers/versionSMITE.m
@@ -2,8 +2,9 @@ function version = versionSMITE()
 % The version (or release) of SMITE currenly being used.
 
    %version = 'development';
-   %version = '1.0.0';
+   %version = '1.0.0';   % JOSS version
    %version = '1.0.1';
-   version = '1.0.2';
+   %version = '1.0.2';
+   version = '1.0.3';   % changed Hierarchal BaGoL
 
 end
