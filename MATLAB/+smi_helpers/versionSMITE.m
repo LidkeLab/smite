8e4e92ce3aaba71e3f294783706d20b0fdb2814b
Michael Wester <wester@math.unm.edu>
2024-06-07 11:31:58 -0600

    Incremement version.

--- MATLAB/+smi_helpers/versionSMITE.m
+++ MATLAB/+smi_helpers/versionSMITE.m
@@ -8,5 +8,6 @@ function version = versionSMITE()
    %version = '1.0.3';   % changed Hierarchal BaGoL
    %version = '1.0.4';   % added circularity, convexity, solidity statistics
    %version = '1.0.5';   % handling of SMLM gui and LocalizeData/FindROI movies
-   version = '1.1.0';   % new style HDF5 files allowed
+   %version = '1.1.0';   % new style HDF5 files allowed
+   version = '1.1.1';   % plotROI can now handle SR files
 end
